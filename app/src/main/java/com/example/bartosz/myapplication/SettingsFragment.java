11d36d54ed2bb13f9af831fa0c72a7701e5a227f
Bartosz.Kosciug <bartosz.kosciug@gmail.com>
2019-01-09 16:34:40 +0100

    pre-final version

---
+++ app/src/main/java/com/example/bartosz/myapplication/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.example.bartosz.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_screen, rootKey);
+    }
+}
